8bc85a0b806b91c2b13ab2ffa15563121bec13b0
nikhilrathore999 <nikhilrathore999@gmail.com>
2022-02-28 14:54:02 +0530

    first commit

---
+++ src/Maincomponent.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import './style.css'
+
+class Maincomponents extends React.Component {
+    state = {
+        name: "",
+        department: "",
+        rating: "",
+        empdetails: []
+    }
+    updateinput = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+    onsubmit = (event) => {
+        event.preventDefault()
+
+        const Objtemp = {
+            name: this.state.name,
+            department: this.state.department,
+            rating: this.state.rating
+
+        }
+
+        const arraytemp = this.state.empdetails;
+        arraytemp.push(Objtemp);
+        this.setState({ empdetails: arraytemp });
+    }
+
+    render() {
+        return (
+            <>
+                 <div id="center">
+                <h1 id="headstyle">EMPLOYEE FEEDBACK FORM</h1>
+                </div><br/>
+                <div id="containerform">
+                <form  onSubmit={this.onsubmit}>
+                <br/><br/>
+                    &nbsp;&nbsp; <label for="name" id="namestyle">Name</label><br/><br/>
+                    &nbsp;&nbsp;<input type="text" id='name' name='name' onChange={this.updateinput} placeholder='Enter Name ' value={this.state.name} required /><br/><br/>
+                    &nbsp;&nbsp;<label for="dept" id="deptstyle">Department</label><br/><br/>
+                    &nbsp;&nbsp;<input type="text" id='dept' name='department' onChange={this.updateinput} placeholder='Enter Department' value={this.state.department} required /><br/><br/>
+                    &nbsp;&nbsp;<label for="rating" id="ratestyle">Rating</label><br/><br/>
+                    &nbsp;&nbsp;<input type="number" id='rating' name='rating' onChange={this.updateinput} placeholder='Enter Rating' value={this.state.rating} required /><br/><br/>
+                    &nbsp;&nbsp;<button id="sub" type="submit" >Submit</button><br/><br/>
+                   
+                </form>
+                </div>
+                <div id="flex">
+                    {this.state.empdetails.map((value, index) => {
+
+                        return (
+
+                            <p id="details">
+                                Name: {value.name} || Department:{value.department} || Rating:{value.rating}
+                            </ p>
+
+                        )
+
+                    })}
+                </div>
+
+            </>
+        )
+    }
+}
+
+export default Maincomponents;
